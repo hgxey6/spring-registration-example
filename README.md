10465aa04013cc130ecc8a0d4880e2e0b43e8d75
hgxey6 <hgxey6@gmail.com>
2021-04-11 15:47:17 +0500

    init commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# spring-registration-example
---
+++ src/test/java/edu/education/registration/SpringRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.education.registration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRegistrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
