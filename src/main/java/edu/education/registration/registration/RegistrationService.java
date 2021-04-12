85cf70acecc23d8b4b1be77b86a310ce25049bdf
hgxey6 <hgxey6@gmail.com>
2021-04-12 16:49:34 +0500

    add and configure registration process

---
+++ src/main/java/edu/education/registration/registration/RegistrationService.java
@@ -0,0 +1,12 @@
+package edu.education.registration.registration;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class RegistrationService {
+
+
+    public String register(RegistrationRequest request) {
+        return "works";
+    }
+}
---
+++ src/main/java/edu/education/registration/security/PasswordEncoder.java
@@ -0,0 +1,11 @@
+package edu.education.registration.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class PasswordEncoder {
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
